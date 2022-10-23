afe921f6cbd73f853d4ed30352624d6ff0531868
Lucas Fernandez <lucasfernandezepe@gmail.com>
2022-10-23 14:12:26 -0300

    convertir tipo de datos

--- Program.cs
+++ Program.cs
@@ -3,3 +3,11 @@ string nombre = Console.ReadLine();
 Console.WriteLine("Hola " + nombre);
 
 Console.WriteLine(DateTime.Now);
+
+string str = "33";
+int entero = Convert.ToInt32(str);
+int sumado = entero+1;
+
+Console.WriteLine("str: " + str);
+Console.WriteLine("sumado: " + sumado);
+
